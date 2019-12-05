b3130e811b8fb85e9f0ec13ef6e3760cf5e29c80
Mohamed Magdy <midomagdy93@gmail.com>
2019-12-05 15:31:06 +0200

    using React

---
+++ precache-manifest.84f1b123b145346d32ef4da1632ca848.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4f6f46afa9fa03922131d4a89ae9bc4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bfc1fba3115faa85c06b",
+    "url": "/static/css/main.7aeca06c.chunk.css"
+  },
+  {
+    "revision": "f64bdcfb52aceade34c4",
+    "url": "/static/js/2.9acbea05.chunk.js"
+  },
+  {
+    "revision": "bfc1fba3115faa85c06b",
+    "url": "/static/js/main.fdd47391.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  }
+]);
