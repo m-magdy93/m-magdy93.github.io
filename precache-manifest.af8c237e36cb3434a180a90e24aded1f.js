93c3b7647f8bfa71e8f5cdd7ef14a009addd80d0
Mohamed Magdy <midomagdy93@gmail.com>
2020-01-04 12:44:03 +0200

    Updates

---
+++ precache-manifest.af8c237e36cb3434a180a90e24aded1f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89aa6a856c798a1ce4375e5d46b4f136",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71171bb756f5e551abf4",
+    "url": "/static/css/main.d1da6dc2.chunk.css"
+  },
+  {
+    "revision": "f64bdcfb52aceade34c4",
+    "url": "/static/js/2.9acbea05.chunk.js"
+  },
+  {
+    "revision": "71171bb756f5e551abf4",
+    "url": "/static/js/main.9e64ee1b.chunk.js"
+  },
+  {
+    "revision": "4c40b94f0748754179e8",
+    "url": "/static/js/runtime-main.bbc06ef5.js"
+  }
+]);
